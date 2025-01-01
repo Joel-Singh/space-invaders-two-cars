2451cca1eb244987db1ff0cd92ab37e9991295a3
Joel Singh <joelsingh788@gmail.com>
2025-01-01 15:00:08 -0500

    Add Cannon

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use bevy::{color::palettes::tailwind::PINK_800, prelude::*};
+use bevy::{
+    color::palettes::tailwind::{GRAY_400, PINK_800},
+    prelude::*,
+};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 mod spawning_enemies;
@@ -13,8 +16,30 @@ fn main() {
             WorldInspectorPlugin::new(),
             spawning_enemies,
         ))
-        .add_systems(Startup, setup_camera)
+        .add_systems(Startup, (setup_camera, spawn_cannon))
         .run();
+}
+
+fn spawn_cannon(mut commands: Commands) {
+    commands
+        .spawn((
+            Node {
+                width: Val::Vw(100.0),
+                margin: UiRect::top(Val::Auto),
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            Name::new("Cannon Container"),
+        ))
+        .with_child((
+            Node {
+                width: Val::Px(50.0),
+                height: Val::Px(100.0),
+                ..default()
+            },
+            BackgroundColor(GRAY_400.into()),
+            Name::new("Cannon"),
+        ));
 }
 
 fn setup_camera(mut commands: Commands) {
